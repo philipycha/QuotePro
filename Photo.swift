f2b858905144ee21f0a96f68e0e7f69ed9d22934
philipycha <philip.yc.ha@gmail.com>
2016-10-12 11:09:30 -0700

    initial commit

---
+++ Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  QuotePro
+//
+//  Created by Philip Ha on 2016-10-12.
+//  Copyright © 2016 Philip Ha. All rights reserved.
+//
+
+import UIKit
+
+class Photo: NSObject {
+
+}
